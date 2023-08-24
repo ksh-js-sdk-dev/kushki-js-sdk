ac6815ae2db3b40f7443ccbeccffef230fd90bf7
Brahian Zapata <brahian.zapata@kushkipagos.com>
2023-08-24 11:01:30 -0500

    feat: [COS-2080] fix styles in inputs
    feat: [COS-2080] fix styles in inputs

--- src/showcase/containers/Checkout.tsx
+++ src/showcase/containers/Checkout.tsx
@@ -8,8 +8,15 @@ import { useEffect, useState } from "react";
 
 export const checkoutContainerStyles = {
   button: {
+    "background-color": "#39a1f4",
+    border: "none",
     borderRadius: "12px",
-    marginLeft: "15px"
+    color: "#FFF",
+    height: "36px",
+    margin: "6px 0",
+    marginLeft: "15px",
+    overflow: "hidden",
+    padding: "0 26px"
   },
   contentCheckout: {
     "align-items": "start",
@@ -19,8 +26,8 @@ export const checkoutContainerStyles = {
   },
   contentTitle: {
     display: "flex",
-    "justify-content": "center",
-    "margin-right": "250px",
+    justifyContent: "center",
+    marginRight: "250px",
     width: "100%"
   }
 };
@@ -34,11 +41,16 @@ export const CheckoutContainer = () => {
       cardHolderName: {
         fieldType: "inputBase",
         inputType: "text",
+        label: "Card holder name",
+        placeholder: "Card holder name",
         selector: "cardHolderName_id",
         styles: {
+          container: {
+            position: "relative"
+          },
           input: {
             border: "1px solid #ccc",
-            borderRadius: "4px",
+            borderRadius: "10px",
             fontFamily: "IBM Plex sans-serif",
             fontSize: "16px",
             fontWeight: "400",
@@ -48,7 +60,7 @@ export const CheckoutContainer = () => {
           },
           inputActive: {
             border: "1px solid #1E65AE",
-            borderRadius: "4px",
+            borderRadius: "10px",
             fontFamily: "IBM Plex sans-serif",
             fontSize: "16px",
             fontWeight: "400",
@@ -62,7 +74,7 @@ export const CheckoutContainer = () => {
             fontFamily: "IBM Plex sans-serif",
             fontSize: "12px",
             fontWeight: "400",
-            left: "10px",
+            left: "16px",
             paddingLeft: "5px",
             paddingRight: "5px",
             position: "absolute",
@@ -78,9 +90,12 @@ export const CheckoutContainer = () => {
         placeholder: "Número de tarjeta",
         selector: "cardNumber_id",
         styles: {
+          container: {
+            position: "relative"
+          },
           input: {
             border: "1px solid #ccc",
-            borderRadius: "4px",
+            borderRadius: "10px",
             fontFamily: "IBM Plex sans-serif",
             fontSize: "16px",
             fontWeight: "400",
@@ -90,7 +105,7 @@ export const CheckoutContainer = () => {
           },
           inputActive: {
             border: "1px solid #1E65AE",
-            borderRadius: "4px",
+            borderRadius: "10px",
             fontFamily: "IBM Plex sans-serif",
             fontSize: "16px",
             fontWeight: "400",
@@ -104,7 +119,7 @@ export const CheckoutContainer = () => {
             fontFamily: "IBM Plex sans-serif",
             fontSize: "12px",
             fontWeight: "400",
-            left: "10px",
+            left: "16px",
             paddingLeft: "5px",
             paddingRight: "5px",
             position: "absolute",
@@ -115,11 +130,16 @@ export const CheckoutContainer = () => {
       cvv: {
         fieldType: "inputBase",
         inputType: "password",
+        label: "CVV",
+        placeholder: "CVV",
         selector: "cvv_id",
         styles: {
+          container: {
+            position: "relative"
+          },
           input: {
             border: "1px solid #ccc",
-            borderRadius: "4px",
+            borderRadius: "10px",
             fontFamily: "IBM Plex sans-serif",
             fontSize: "16px",
             fontWeight: "400",
@@ -129,7 +149,7 @@ export const CheckoutContainer = () => {
           },
           inputActive: {
             border: "1px solid #1E65AE",
-            borderRadius: "4px",
+            borderRadius: "10px",
             fontFamily: "IBM Plex sans-serif",
             fontSize: "16px",
             fontWeight: "400",
@@ -143,7 +163,51 @@ export const CheckoutContainer = () => {
             fontFamily: "IBM Plex sans-serif",
             fontSize: "12px",
             fontWeight: "400",
-            left: "10px",
+            left: "16px",
+            paddingLeft: "5px",
+            paddingRight: "5px",
+            position: "absolute",
+            top: "-7px"
+          }
+        }
+      },
+      deferred: {
+        fieldType: "inputBase",
+        inputType: "text",
+        label: "Diferido",
+        placeholder: "Diferido",
+        selector: "deferred_id",
+        styles: {
+          container: {
+            position: "relative"
+          },
+          input: {
+            border: "1px solid #ccc",
+            borderRadius: "10px",
+            fontFamily: "IBM Plex sans-serif",
+            fontSize: "16px",
+            fontWeight: "400",
+            outline: "none",
+            padding: "10px",
+            width: "350px"
+          },
+          inputActive: {
+            border: "1px solid #1E65AE",
+            borderRadius: "10px",
+            fontFamily: "IBM Plex sans-serif",
+            fontSize: "16px",
+            fontWeight: "400",
+            outline: "none",
+            padding: "10px",
+            width: "350px"
+          },
+          label: {
+            background: "white",
+            color: "#6D7781",
+            fontFamily: "IBM Plex sans-serif",
+            fontSize: "12px",
+            fontWeight: "400",
+            left: "16px",
             paddingLeft: "5px",
             paddingRight: "5px",
             position: "absolute",
@@ -154,11 +218,16 @@ export const CheckoutContainer = () => {
       expirationDate: {
         fieldType: "inputBase",
         inputType: "text",
+        label: "Fecha de vencimiento",
+        placeholder: "Fecha de vencimiento",
         selector: "expirationDate_id",
         styles: {
+          container: {
+            position: "relative"
+          },
           input: {
             border: "1px solid #ccc",
-            borderRadius: "4px",
+            borderRadius: "10px",
             fontFamily: "IBM Plex sans-serif",
             fontSize: "16px",
             fontWeight: "400",
@@ -168,7 +237,7 @@ export const CheckoutContainer = () => {
           },
           inputActive: {
             border: "1px solid #1E65AE",
-            borderRadius: "4px",
+            borderRadius: "10px",
             fontFamily: "IBM Plex sans-serif",
             fontSize: "16px",
             fontWeight: "400",
@@ -182,47 +251,7 @@ export const CheckoutContainer = () => {
             fontFamily: "IBM Plex sans-serif",
             fontSize: "12px",
             fontWeight: "400",
-            left: "10px",
-            paddingLeft: "5px",
-            paddingRight: "5px",
-            position: "absolute",
-            top: "-7px"
-          }
-        }
-      },
-      // eslint-disable-next-line sort-keys
-      deferred: {
-        fieldType: "inputBase",
-        inputType: "text",
-        selector: "deferred_id",
-        styles: {
-          input: {
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-            fontFamily: "IBM Plex sans-serif",
-            fontSize: "16px",
-            fontWeight: "400",
-            outline: "none",
-            padding: "10px",
-            width: "350px"
-          },
-          inputActive: {
-            border: "1px solid #1E65AE",
-            borderRadius: "4px",
-            fontFamily: "IBM Plex sans-serif",
-            fontSize: "16px",
-            fontWeight: "400",
-            outline: "none",
-            padding: "10px",
-            width: "350px"
-          },
-          label: {
-            background: "white",
-            color: "#6D7781",
-            fontFamily: "IBM Plex sans-serif",
-            fontSize: "12px",
-            fontWeight: "400",
-            left: "10px",
+            left: "16px",
             paddingLeft: "5px",
             paddingRight: "5px",
             position: "absolute",
@@ -256,12 +285,11 @@ export const CheckoutContainer = () => {
       <div style={checkoutContainerStyles.contentCheckout!}>
         <div id="cardHolderName_id"></div>
         <div id="cardNumber_id"></div>
-        <div id="cvv_id"></div>
         <div id="expirationDate_id"></div>
+        <div id="cvv_id"></div>
         <div id="deferred_id"></div>
 
         <button
-          className="mui-btn mui-btn--primary mui-btn--raised"
           style={checkoutContainerStyles.button!}
           data-testid="tokenRequestBtn"
           onClick={() => getToken()}
--- src/showcase/main.tsx
+++ src/showcase/main.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "../styles/mui.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // TODO: Validate double render with zoid
