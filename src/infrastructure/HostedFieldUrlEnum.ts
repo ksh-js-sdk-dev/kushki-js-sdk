38ee13595b6991f9b07e6d267e5ac247041df676
Esteban Aristizábal <esteban@kushkipagos.com>
2023-10-23 10:32:20 -0300

    feat(COS-14195): update pipeline

--- src/infrastructure/HostedFieldUrlEnum.ts
+++ src/infrastructure/HostedFieldUrlEnum.ts
@@ -1,3 +1,3 @@
 export enum HostedFieldUrlEnum {
-  LOCAL_SPA_URL = "https://qa-spa-js-inputs.kushkipagos.blue" // change to dev locally
+  LOCAL_SPA_URL = "https://qa-spa-js-inputs.kushkipagos.blue/inputs" // change to dev locally
 }
