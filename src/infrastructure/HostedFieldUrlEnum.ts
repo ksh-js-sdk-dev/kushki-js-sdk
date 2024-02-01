d4258fc010d6e55ab260ea1208c6f292e4a57b8e
Santiago Cuascota <santiago.cuascota@kushkipagos.com>
2024-02-01 12:54:09 -0500

    Update for primary

--- src/infrastructure/HostedFieldUrlEnum.ts
+++ src/infrastructure/HostedFieldUrlEnum.ts
@@ -1,3 +1,3 @@
 export enum HostedFieldUrlEnum {
-  LOCAL_SPA_URL = "https://uat-spa-js-inputs.kushkipagos.ninja/inputs" // change to dev locally
+  LOCAL_SPA_URL = "https://spa-js-inputs.kushkipagos.com/inputs" // change to dev locally
 }
