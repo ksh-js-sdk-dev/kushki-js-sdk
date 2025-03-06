60075f1c0077ed26986e353f29d1a46402e18bce
Santiago Cuascota <santiago.cuascota@kushkipagos.com>
2025-03-06 11:37:47 -0500

    update env vars

--- src/infrastructure/EnvironmentEnum.ts
+++ src/infrastructure/EnvironmentEnum.ts
@@ -2,7 +2,7 @@
  * Environment enum file
  */
 export enum EnvironmentEnum {
-  uat = "https://api-qa.orizaba.kushki.dev/",
+  uat = "https://api-uat.kushkipagos.com/",
   prod = "https://api.kushkipagos.com/",
   rollbarId = "38ce2c1dbb3d4055a44c3c324ee9d993"
 }
