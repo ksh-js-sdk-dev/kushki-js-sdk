0384ced356df8a2f6ca65eba0c3a72206a6445e9
Francisco David Munoz <david.munoz@kushkipagos.com>
2026-02-09 12:10:17 -0500

    [CAR-15756] Restore the uat endpoint

--- src/infrastructure/EnvironmentEnum.ts
+++ src/infrastructure/EnvironmentEnum.ts
@@ -2,9 +2,8 @@
  * Environment enum file
  */
 
-// https://api-uat.kushkipagos.com/
 export enum EnvironmentEnum {
-  uat = "https://ecomm-dev-01-us-east-1.kushki.dev/",
+  uat = "https://api-uat.kushkipagos.com/",
   prod = "https://api.kushkipagos.com/",
   rollbarId = "38ce2c1dbb3d4055a44c3c324ee9d993"
 }
